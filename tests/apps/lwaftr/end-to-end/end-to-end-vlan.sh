1aef39e72c4801aa8041ea11ca0a2b37ca0aba1a
Katerina Barone-Adesi <kbarone@igalia.com>
2015-11-20 19:17:55 +0000

    Enabled the rest of the vlan tests

--- tests/apps/lwaftr/end-to-end/end-to-end-vlan.sh
+++ tests/apps/lwaftr/end-to-end/end-to-end-vlan.sh
@@ -42,34 +42,30 @@ function snabb_run_and_cmp {
    echo "Test passed"
 }
 
-# Fail
-# echo "Testing: from-internet IPv4 packet found in the binding table."
-# snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
-#    ${TEST_DATA}/tcp-frominet-bound.pcap ${EMPTY} \
-#    ${EMPTY} ${TEST_DATA}/tcp-afteraftr-ipv6.pcap
+echo "Testing: from-internet IPv4 packet found in the binding table."
+snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
+   ${TEST_DATA}/tcp-frominet-bound.pcap ${EMPTY} \
+   ${EMPTY} ${TEST_DATA}/tcp-afteraftr-ipv6.pcap
 
-# Fail
-# echo "Testing: traffic class mapping"
-# snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
-#    ${TEST_DATA}/tcp-frominet-trafficclass.pcap ${EMPTY} \
-#    ${EMPTY} ${TEST_DATA}/tcp-afteraftr-ipv6-trafficclass.pcap
+echo "Testing: traffic class mapping"
+snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
+   ${TEST_DATA}/tcp-frominet-trafficclass.pcap ${EMPTY} \
+   ${EMPTY} ${TEST_DATA}/tcp-afteraftr-ipv6-trafficclass.pcap
 
 echo "Testing: from-internet IPv4 packet found in the binding table, original TTL=1."
 snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
    ${TEST_DATA}/tcp-frominet-bound-ttl1.pcap ${EMPTY}\
    ${TEST_DATA}/icmpv4-time-expired.pcap ${EMPTY}
 
-# Fail
-# echo "Testing: from-B4 IPv4 fragmentation (2)"
-# snabb_run_and_cmp ${TEST_CONF}/small_ipv4_mtu_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-ipv6-fromb4-toinet-1046.pcap \
-#    ${TEST_DATA}/tcp-ipv4-toinet-2fragments.pcap ${EMPTY}
+echo "Testing: from-B4 IPv4 fragmentation (2)"
+snabb_run_and_cmp ${TEST_CONF}/small_ipv4_mtu_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-ipv6-fromb4-toinet-1046.pcap \
+   ${TEST_DATA}/tcp-ipv4-toinet-2fragments.pcap ${EMPTY}
 
-# Fail
-# echo "Testing: from-B4 IPv4 fragmentation (3)"
-# snabb_run_and_cmp ${TEST_CONF}/small_ipv4_mtu_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-ipv6-fromb4-toinet-1500.pcap \
-#    ${TEST_DATA}/tcp-ipv4-toinet-3fragments.pcap ${EMPTY}
+echo "Testing: from-B4 IPv4 fragmentation (3)"
+snabb_run_and_cmp ${TEST_CONF}/small_ipv4_mtu_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-ipv6-fromb4-toinet-1500.pcap \
+   ${TEST_DATA}/tcp-ipv4-toinet-3fragments.pcap ${EMPTY}
 
 echo "Testing: from-internet IPv4 packet found in the binding table, needs IPv6 fragmentation (2)."
 snabb_run_and_cmp ${TEST_CONF}/small_ipv6_mtu_no_icmp_vlan.conf \
@@ -81,11 +77,10 @@ snabb_run_and_cmp ${TEST_CONF}/small_ipv6_mtu_no_icmp_vlan.conf \
    ${TEST_DATA}/tcp-frominet-bound-2734.pcap ${EMPTY} \
    ${EMPTY} ${TEST_DATA}/tcp-afteraftr-ipv6-3frags.pcap
 
-# Fail
-# echo "Testing: IPv6 reassembly (followed by decapsulation)."
-# snabb_run_and_cmp ${TEST_CONF}/small_ipv6_mtu_no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-ipv6-2frags-bound.pcap \
-#    ${TEST_DATA}/tcp-ipv4-2ipv6frags-reassembled.pcap ${EMPTY}
+echo "Testing: IPv6 reassembly (followed by decapsulation)."
+snabb_run_and_cmp ${TEST_CONF}/small_ipv6_mtu_no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-ipv6-2frags-bound.pcap \
+   ${TEST_DATA}/tcp-ipv4-2ipv6frags-reassembled.pcap ${EMPTY}
 
 echo "Testing: from-internet IPv4 packet found in the binding table, needs IPv6 fragmentation, DF set, ICMP-3,4."
 snabb_run_and_cmp ${TEST_CONF}/small_ipv6_mtu_no_icmp_vlan.conf \
@@ -107,11 +102,10 @@ snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
    ${TEST_DATA}/tcp-afteraftr-ipv6.pcap ${EMPTY} \
    ${EMPTY} ${EMPTY}
 
-# Fail
-# echo "Testing: from-b4 to-internet IPv6 packet found in the binding table."
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-ipv6.pcap \
-#    ${TEST_DATA}/decap-ipv4.pcap ${EMPTY}
+echo "Testing: from-b4 to-internet IPv6 packet found in the binding table."
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-ipv6.pcap \
+   ${TEST_DATA}/decap-ipv4.pcap ${EMPTY}
 
 echo "Testing: from-b4 to-internet IPv6 packet NOT found in the binding table, no ICMP"
 snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
@@ -123,77 +117,66 @@ snabb_run_and_cmp ${TEST_CONF}/icmp_on_fail_vlan.conf \
    ${EMPTY} ${TEST_DATA}/tcp-fromb4-ipv6-unbound.pcap \
    ${EMPTY} ${TEST_DATA}/icmpv6-nogress.pcap
 
-# Fail
-# echo "Testing: from-to-b4 IPv6 packet, no hairpinning"
+echo "Testing: from-to-b4 IPv6 packet, no hairpinning"
 # The idea is that with hairpinning off, the packet goes out the inet interface
 # and something else routes it back for re-encapsulation. It's not clear why
 # this would be desired behaviour, but it's my reading of the RFC.
-# snabb_run_and_cmp ${TEST_CONF}/no_hairpin_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-ipv6.pcap \
-#    ${TEST_DATA}/decap-ipv4-nohair.pcap ${EMPTY}
+snabb_run_and_cmp ${TEST_CONF}/no_hairpin_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-ipv6.pcap \
+   ${TEST_DATA}/decap-ipv4-nohair.pcap ${EMPTY}
 
-# Fail
-# echo "Testing: from-to-b4 IPv6 packet, with hairpinning"
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-ipv6.pcap \
-#    ${EMPTY} ${TEST_DATA}/recap-ipv6.pcap
+echo "Testing: from-to-b4 IPv6 packet, with hairpinning"
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-ipv6.pcap \
+   ${EMPTY} ${TEST_DATA}/recap-ipv6.pcap
 
-# Fail
-# echo "Testing: from-b4 IPv6 packet, with hairpinning, to B4 with custom lwAFTR address"
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-customBRIP-ipv6.pcap \
-#    ${EMPTY} ${TEST_DATA}/recap-tocustom-BRIP-ipv6.pcap
+echo "Testing: from-b4 IPv6 packet, with hairpinning, to B4 with custom lwAFTR address"
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-tob4-customBRIP-ipv6.pcap \
+   ${EMPTY} ${TEST_DATA}/recap-tocustom-BRIP-ipv6.pcap
 
-# Fail
-# echo "Testing: from-b4 IPv6 packet, with hairpinning, from B4 with custom lwAFTR address"
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-customBRIP-tob4-ipv6.pcap \
-#    ${EMPTY} ${TEST_DATA}/recap-fromcustom-BRIP-ipv6.pcap
+echo "Testing: from-b4 IPv6 packet, with hairpinning, from B4 with custom lwAFTR address"
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-customBRIP-tob4-ipv6.pcap \
+   ${EMPTY} ${TEST_DATA}/recap-fromcustom-BRIP-ipv6.pcap
 
-# Fail
-# echo "Testing: from-b4 IPv6 packet, with hairpinning, different non-default lwAFTR addresses"
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${EMPTY} ${TEST_DATA}/tcp-fromb4-customBRIP1-tob4-customBRIP2-ipv6.pcap \
-#    ${EMPTY} ${TEST_DATA}/recap-customBR-IPs-ipv6.pcap
+echo "Testing: from-b4 IPv6 packet, with hairpinning, different non-default lwAFTR addresses"
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${EMPTY} ${TEST_DATA}/tcp-fromb4-customBRIP1-tob4-customBRIP2-ipv6.pcap \
+   ${EMPTY} ${TEST_DATA}/recap-customBR-IPs-ipv6.pcap
 
 # Test UDP input
 
-# Fail
 # Test ICMP inputs (with and without drop policy)
-# echo "Testing: incoming ICMPv4 echo request, matches binding table"
-# snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-echo-request.pcap ${EMPTY} \
-#    ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-echo-request.pcap
+echo "Testing: incoming ICMPv4 echo request, matches binding table"
+snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-echo-request.pcap ${EMPTY} \
+   ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-echo-request.pcap
 
-# Fail
-# echo "Testing: incoming ICMPv4 echo request, matches binding table"
-# snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-echo-request-invalid-icmp-checksum.pcap ${EMPTY} \
-#    ${EMPTY} ${EMPTY}
+echo "Testing: incoming ICMPv4 echo request, matches binding table"
+snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-echo-request-invalid-icmp-checksum.pcap ${EMPTY} \
+   ${EMPTY} ${EMPTY}
 
-# Fail
-# echo "Testing: incoming ICMPv4 echo request, matches binding table, dropping ICMP"
-# snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-echo-request.pcap ${EMPTY} \
-#    ${EMPTY} ${EMPTY}
+echo "Testing: incoming ICMPv4 echo request, matches binding table, dropping ICMP"
+snabb_run_and_cmp ${TEST_CONF}/no_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-echo-request.pcap ${EMPTY} \
+   ${EMPTY} ${EMPTY}
 
-# Fail
-# echo "Testing: incoming ICMPv4 echo request, doesn't match binding table"
-# snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-echo-request-unbound.pcap ${EMPTY} \
-#    ${EMPTY} ${EMPTY}
+echo "Testing: incoming ICMPv4 echo request, doesn't match binding table"
+snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-echo-request-unbound.pcap ${EMPTY} \
+   ${EMPTY} ${EMPTY}
 
-# Fail
-# echo "Testing: incoming ICMPv4 echo reply, matches binding table"
-# snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-echo-reply.pcap ${EMPTY} \
-#    ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-echo-reply.pcap
+echo "Testing: incoming ICMPv4 echo reply, matches binding table"
+snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-echo-reply.pcap ${EMPTY} \
+   ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-echo-reply.pcap
 
-# Fail
-# echo "Testing: incoming ICMPv4 3,4 'too big' notification, matches binding table"
-# snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
-#    ${TEST_DATA}/incoming-icmpv4-34toobig.pcap ${EMPTY} \
-#    ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-34toobig.pcap
+echo "Testing: incoming ICMPv4 3,4 'too big' notification, matches binding table"
+snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \
+   ${TEST_DATA}/incoming-icmpv4-34toobig.pcap ${EMPTY} \
+   ${EMPTY} ${TEST_DATA}/ipv6-tunneled-incoming-icmpv4-34toobig.pcap
 
 echo "Testing: incoming ICMPv6 1,3 destination/address unreachable, OPE from internet"
 snabb_run_and_cmp ${TEST_CONF}/tunnel_icmp_vlan.conf \

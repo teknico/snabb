d3939dc99634968a48d4fac12dbddaa895bb5752
Andy Wingo <wingo@igalia.com>
2016-06-07 13:27:04 +0000

    Add some headroom before a packet's data.
    This allows virtio to scribble over the headroom with its header.

--- src/core/packet.h
+++ src/core/packet.h
@@ -1,8 +1,14 @@
-// The maximum amount of payload in any given packet.
-enum { PACKET_PAYLOAD_SIZE = 10*1024 };
+enum {
+    // A few bytes of headroom if needed by a low-level transport like
+    // virtio.
+    PACKET_HEADROOM_SIZE = 32,
+    // The maximum amount of payload in any given packet.
+    PACKET_PAYLOAD_SIZE = 10*1024
+};
 
 // Packet of network data, with associated metadata.
 struct packet {
+    unsigned char headroom[PACKET_HEADROOM_SIZE];
     unsigned char data[PACKET_PAYLOAD_SIZE];
     uint16_t length;           // data payload length
 };
